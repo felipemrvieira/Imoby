d30edf951d29ccede4cdd02010a20ad553a49da1
felipemaciel <felipemrvieira@gmail.com>
2017-09-12 16:24:10 -0300

    remocao do admin e capturas

--- admin/logica/loga.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($_POST['usuario'] == 'admin' && $_POST['senha'] == 'rox'){
-    session_start();
-    $_SESSION['usuarioLogado'] = 'admin';
-    header("location: ../admin.php");
-}else{
-     header("location: ../login.php");
-}
